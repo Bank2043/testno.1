a402f7ab52797c4d0df8357dd39aef0e32035a32
unknown <thanet2715chailert@gmail.com>
2022-07-17 21:58:08 +0700

    first commit

---
+++ pro.php
@@ -0,0 +1,63 @@
+<div class="nav-left-sidebar sidebar-dark">
+            <div class="menu-list">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav flex-column">
+                            <li class="nav-divider">
+                                Menu
+                            </li>
+
+                           
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-3" aria-controls="submenu-3"><i class="fas fa-fw fa-chart-pie"></i>จัดการประเภทสินค้า</a>
+                                <div id="submenu-3" class="collapse submenu" >
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="type.php">จัดการประเภทสินค้า</a>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </li>
+
+
+
+
+
+
+
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-4" aria-controls="submenu-4"><i class="fab fa-fw fa-wpforms"></i>จัดการสินค้า</a>
+                                <div id="submenu-4" class="collapse submenu" >
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="product.php">จัดการสินค้า</a>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </li>
+
+
+
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-5" aria-controls="submenu-5"><i class="fas fa-fw fa-table"></i>รายการสั่งสินค้า</a>
+                                <div id="submenu-5" class="collapse submenu" >
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="status.php">จัดการรายการสั่งสินค้า</a>
+                                        </li>
+                                        
+                                    </ul>
+                                </div>
+                            </li>
+
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+declare function get(): any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'my-app';
+  ngOnInit(): void {
+    // call the externaljs functions
+    get(); // without param
 }
+}
+//external js function declaration
+
+
